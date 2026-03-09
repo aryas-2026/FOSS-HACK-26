be112a14adc9401a3714aed26cf1043c32971cc2
fadil2006-sp <fadilmohammed.n2024@vitstudent.ac.in>
2026-03-09 22:01:44 +0530

    Day 3: Implemented rule-based adaptive cruise control safety logic

--- main.py
+++ main.py
@@ -1,65 +1,59 @@
 import time
 
 def main():
-    print("\nAdaptive Cruise Control Simulation Started:\n ")
 
-    position=0.0
-    dt=0.02 #50 Hz
-    Velocity = 5.0
+    print("\nACC Simulation Started (Day 3: Safety Rule)\n")
 
-    for step in range(1,101):
-
-        position+=Velocity*dt
-        print(f"Step {step}--> Position= {position:.3f}m")
-        time.sleep(dt)
-
-    print("\nSimulation Finished\n")
-
-if __name__=="__main__":
-    main()
-
-import time
-
-def main():
-
-    print("\nACC Simulation for Two Vehicles Started\n")
-
-    
     # Simulation parameters
-    
-    dt = 0.02      # 50 Hz control loop
+    dt = 0.02     # 50 Hz
     steps = 150
 
+    # Safety parameters
+    max_acc = 1.5
+    max_brake = -3.0
+    safe_distance = 15.0
 
-    # Target Vehicle (front car)
-    
-    target_position = 30.0     # meters ahead
-    target_velocity = 8.0      # m/s
+    # Target Vehicle
+    target_position = 30.0
+    target_velocity = 8.0
 
-    
-    # Controlled Vehicle (our car)
-    
+    # Controlled Vehicle
     controlled_position = 0.0
-    controlled_velocity = 5.0
+    controlled_velocity = 6.0
+    controlled_acc = 0.0
 
     for step in range(1, steps + 1):
 
-        # Target vehicle slows down suddenly
-        if step > 80:
+        # Target vehicle slows down
+        if step > 120:
             target_velocity = 4.0
 
-        # Update positions
+        # Update target vehicle
         target_position += target_velocity * dt
-        controlled_position += controlled_velocity * dt
 
         # Distance between vehicles
         distance = target_position - controlled_position
 
+        # Safety Rule (ACC logic)
+        if distance < safe_distance:
+            controlled_acc = max_brake
+        else:
+            controlled_acc = max_acc
+
+        # Update controlled vehicle
+        controlled_velocity += controlled_acc * dt
+
+        if controlled_velocity < 0:
+            controlled_velocity = 0
+
+        controlled_position += controlled_velocity * dt
+
         print(
-            f"Step {step:03d}   "
-            f"Distance: {distance:6.2f} m   "
-            f"Target Pos: {target_position:6.2f} m   "
-            f"Controlled Pos: {controlled_position:6.2f} m"
+            f"Step {step:03d} | "
+            f"Distance: {distance:.2f} m | "
+            f"Target Pos: {target_position:.2f} m | "
+            f"Ctrl Vel: {controlled_velocity:.2f} m/s | "
+            f"Ctrl Acc: {controlled_acc:.2f} m/s²"
         )
 
         time.sleep(dt)
